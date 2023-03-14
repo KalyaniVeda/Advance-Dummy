514607047c87b9a82eada8e773aff9e45013f017
KalyaniVeda <kalyani.veda@gmail.com>
2023-03-14 16:57:27 +0530

    mycommit

---
+++ MavenSample/src/test/java/practise2/SampleClassTest.java
@@ -0,0 +1,14 @@
+package practise2;
+
+import org.testng.annotations.Test;
+
+
+public class SampleClassTest {
+	
+@Test	
+	public void method1()
+	{
+		System.out.println("Method is running");
+	}
+
+}
