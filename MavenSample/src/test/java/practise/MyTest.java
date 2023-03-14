fa1a2af44cefb2ff2ddbd8a626bdc6660484ff1a
KalyaniVeda <kalyani.veda@gmail.com>
2023-03-14 17:06:11 +0530

    commit

--- MavenSample/src/test/java/practise/MyTest.java
+++ MavenSample/src/test/java/practise/MyTest.java
@@ -3,24 +3,19 @@ package practise;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class Parameters {
-
-	@Test
-	public void method()
+@Test
+public class MyTest {
+	
+	public void m1()
 	{
-		WebDriver driver;
+		WebDriver driver = null;
 		String browser = System.getProperty("browser");
 		String url = System.getProperty("url");
-		
-		//System.out.println(browser);
-		//System.out.println(url);
-		
-		if(browser.equalsIgnoreCase("chrome")) 
+		System.out.println(browser);
+		if(browser.equalsIgnoreCase("browser"))
 		{
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
@@ -30,15 +25,7 @@ public class Parameters {
 			WebDriverManager.edgedriver().setup();
 			driver = new EdgeDriver();
 		}
-		else if(browser.equalsIgnoreCase("firefox"))
-		{
-			WebDriverManager.firefoxdriver().setup();
-		    driver = new FirefoxDriver();
-		}
-		else
-		{
-			driver = new ChromeDriver();
-		}
 		driver.get(url);
 	}
+
 }
